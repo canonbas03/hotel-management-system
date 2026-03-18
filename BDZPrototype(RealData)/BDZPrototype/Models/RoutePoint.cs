e009916338491ca81fc91f079b39c66bfb9cdeca
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-03-18 19:03:53 +0200

    Removed old files

--- BDZPrototype(RealData)/BDZPrototype/Models/RoutePoint.cs
+++
@@ -1,8 +0,0 @@
-﻿public class RoutePoint
-{
-    public string Name { get; set; }
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public double RadiusMeters { get; set; }
-    public string AudioFile { get; set; }
-}
--- HotelMVC/HotelMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HotelMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
