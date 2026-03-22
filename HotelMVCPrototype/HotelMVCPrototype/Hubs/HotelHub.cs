ff9c8a9fa3a11e44d99700ff1ca19946716a28cc
Salih-Dzhan Yonbash <salihcanonbas@gmail.com>
2026-03-22 07:02:07 +0200

    removed unnecessary lines

--- HotelMVCPrototype/HotelMVCPrototype/Hubs/HotelHub.cs
+++ HotelMVCPrototype/HotelMVCPrototype/Hubs/HotelHub.cs
@@ -4,14 +4,5 @@ namespace HotelMVCPrototype.Hubs
 {
     public class HotelHub : Hub
     {
-        //public async Task RoomStatusChanged(int roomId, string newStatusColor)
-        //{
-        //    await Clients.All.SendAsync("RoomStatusChanged", roomId, newStatusColor);
-        //}
-
-        //public async Task RoomStatusChanged(int roomId, string newStatusColor)
-        //{
-        //    await Clients.All.SendAsync("RoomStatusChanged", roomId, newStatusColor);
-        //}
     }
 }
